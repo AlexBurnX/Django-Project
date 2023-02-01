7eb94cae156d564e3705ca2b270ad29402d5210e
AlexBurnX <burning1@mail.ru>
2023-02-01 10:17:33 +0300

    added new pages

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # подключаем ещё приложения
+
     'django.contrib.sites',
     'django.contrib.flatpages',
     'fpages'
